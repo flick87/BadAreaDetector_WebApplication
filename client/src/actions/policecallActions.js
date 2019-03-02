7b6db0a192f2b09d835324019c5e7ca9e710e23a
arodr856 <arodr856@gmail.com>
2019-03-02 12:05:03 -0800

    added actions directory with actions and action types files

---
+++ client/src/actions/policecallActions.js
@@ -0,0 +1,7 @@
+import { GET_POLICECALLS } from './actionTypes';
+
+export const getPoliceCalls = () => {
+    // Dispatch to store and make aware that data is being fetched
+    // fetch data
+    // when data reutrns dispatch to store you have received data and send it as payload
+}
