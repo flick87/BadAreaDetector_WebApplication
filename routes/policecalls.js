c6bddc6a4de2122d1364b473c98c4e7356efb677
arodr856 <arodr856@gmail.com>
2019-04-15 21:54:05 -0700

    partitioned data being pulled from COS

--- routes/policecalls.js
+++ routes/policecalls.js
@@ -5,33 +5,23 @@ const fs = require('fs');
 const partial_data = require('../partial_data');
 
 
-const bucketName = 'rmart167-cos';
-const itemName = 'pd_calls.json';
+// const bucketName = 'rmart167-cos';
+// const itemName = 'pd_calls.json';
+
+const bucketName = 'policecalls';
+const itemName = 'pdcalls_8.json';
 
 router.get('/', (req, res) => {
     PoliceCallsModel.getItem(bucketName, itemName, (err, result) => {
         if(err){
             res.json(err);
         }else{
-            const count = req.query.count;
-            if(count){
-                const d = JSON.parse(result);
-                const send = d.slice(0, count);
-                console.log("send: " + typeof send);
-                res.json(send); 
-            }else{
-                const send = JSON.parse(result).slice(0, 1000);
-                res.json(send);;
-            }
-            
+            res.json(result); 
         }
     });
 });
 
 router.get('/dev', (req, res) => {
-    const count = req.query.count;
-    // console.log(count);
-    console.log(typeof partial_data);
     res.json(partial_data);
 });
 
